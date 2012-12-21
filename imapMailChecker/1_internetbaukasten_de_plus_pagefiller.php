c262fd90f4cb4379bcdcbe6710128d3911251b93
Dmitriy O <dimonweb@gmail.com>
2012-12-21 17:06:36 +0200

    work. but algoritm need reduce

--- imapMailChecker/1_internetbaukasten_de_plus_pagefiller.php
+++ imapMailChecker/1_internetbaukasten_de_plus_pagefiller.php
@@ -5,13 +5,10 @@ $response = $article->post('http://link.gutes-lernen.com/xml/request.php', array
 $articleBody = $response->body;
 $xml = json_decode(json_encode((array) simplexml_load_string($articleBody)), 1);
 
-//query to change page text
+//query to create new pages with head text
 $PageEdit = new Curl;
-
 $response = $PageEdit->get('http://www.internetbaukasten.de/index.php?view=standalone_seiten_ajax');
-
 $pageToEdit = $response->body;
-
 $pageToEdit = str_get_html($pageToEdit);
 $ol = $pageToEdit->find("#seitenlisten_container", 0)->find("ol", 0);
 
@@ -19,7 +16,7 @@ $j=0;
 foreach ($xml['element']['head3'] as $value) {
 	//if pages more than 4 create more pages
 	$j++;
-	if ($j > 2) {
+	if ($j > 1) {
 		//find last id of page to send id
 		$lastchild = $ol -> lastchild() -> id;
 		preg_match_all("/list_(.*)/",$li -> id,$ok_id);
@@ -30,32 +27,35 @@ foreach ($xml['element']['head3'] as $value) {
 }
 
 
+// receive id to new block
+$response = $PageEdit->get('http://www.internetbaukasten.de/index.php?view=bearbeiten');
+$pageToEdit = $response->body;
+$pageToEdit = str_get_html($pageToEdit);
+
+
+
+$ul = $pageToEdit->find("#ibk_nav_ul", 0);
 //receive all links to editable pages
-//foreach($ol->find('li') as $li) 
-//{	
-//	
-//	preg_match_all("/list_(.*)/",$li -> id,$ok_id);
-//    $linksToEdit[] = $ok_id;
-//    
-//}
-//
-//echo $linksToEdit[0][1][0];
-
-
-//$response = $PageEdit->get('http://www.internetbaukasten.de/index.php?aktion=seiten_auswahl&seiten_id='.$linksToEdit[0][1][0]);
-//$pageToEdit = $response->body;
-//print_r($response);
-//$pageToEdit = str_get_html($pageToEdit);
-//$id_postToEdit = $pageToEdit -> find('#ibk_content_container', 0) -> first_child() -> first_child() -> id;
-//
-//preg_match_all("/ibk_block_id_(.*)/",$id_postToEdit,$ok_id);
-//$PageEdit-> get('http://www.internetbaukasten.de/index.php?aktion=edit_block&block_id='.$ok_id[1][0].'&type=edit');
-//$PageEdit->post('http://www.internetbaukasten.de/index.php', array('aktion' => 'edit_block_data', 'ta' => '	<h1>Edited Page222!!!:)</h1>'));
+$k=0;
+foreach($ul->find('li') as $li) 
+{
+    //$linksToEdit[] =  $li -> find('a', 0) -> href;
+    $response = $PageEdit->get('http://www.internetbaukasten.de/index.php?aktion=seiten_auswahl&seiten_id='.$li -> find('a', 0) -> href);
+    $pageToEdit = $response->body;
+	$pageToEdit = str_get_html($pageToEdit);
+	$new_block = $pageToEdit -> find('.ibk_editiable_new', 0)->id;
+	preg_match_all("/ibk_block_id_(.*)_elemente_id_11/",$new_block,$new_block_id);
+	
+	$k++;
+	// create new block
+	$PageEdit -> get('http://www.internetbaukasten.de/index.php?aktion=new_sub_block&block_id='.$new_block_id[1][0].'&elemente_id=1');
+	$PageEdit -> get('http://www.internetbaukasten.de/index.php?aktion=set_block_typ&blockkombination_id=49');
+	$PageEdit -> post('http://www.internetbaukasten.de/index.php', array('aktion' => 'edit_block_data', 'ta' => $xml['element']['text'][$k]));
+}
 
 //export to the web
 $PageEdit->get('http://www.internetbaukasten.de/index.php?aktion=export_do');
 
-//Delete all sites
 
 
 
--- imapMailChecker/1_internetbaukasten_de_test_xml.php
+++ imapMailChecker/1_internetbaukasten_de_test_xml.php
@@ -3,20 +3,19 @@
 $article = new Curl;
 $response = $article->post('http://link.gutes-lernen.com/xml/request.php', array('req_id' => '0000', 'pass' => 'ad6cd7f8413b9b6bc0baaddf62d0ce59', 'get_domain' => 'true', 'dom_purl' => 'sdom.co', 'get_link' => 'true'));
 $articleBody = $response->body;
-
 $xml = json_decode(json_encode((array) simplexml_load_string($articleBody)), 1);
-//print_r($xml);
+print_r($xml);
 
-echo $xml['element']['domain'];
-echo $xml['element']['subpage_url'];
-echo $xml['element']['subpage_title'];
-echo $xml['element']['menu_item'];
-echo $xml['element']['head1'];
-
-foreach ($xml['element']['head3'] as $value) {
-	echo $value;
-}
-
-foreach ($xml['element']['text'] as $value) {
-	echo $value;
-}
+//echo $xml['element']['domain'];
+//echo $xml['element']['subpage_url'];
+//echo $xml['element']['subpage_title'];
+//echo $xml['element']['menu_item'];
+//echo $xml['element']['head1'];
+//
+//foreach ($xml['element']['head3'] as $value) {
+//	echo $value;
+//}
+//
+//foreach ($xml['element']['text'] as $value) {
+//	echo $value;
+//}
